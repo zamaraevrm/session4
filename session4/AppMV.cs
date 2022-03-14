d89bede423f23ceda6b721ae3cd074e8b283a86c
zamaraevrm <zamaraevrm@302D-2>
2022-03-14 12:39:00 +0700

    Добавьте файлы проекта.

---
+++ session4/AppMV.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session4
+{
+    class AppMV
+    {
+        private string connString = @"Data Source=DBSRV\SQL2021;Initial Catalog=Zamaraev_Roman;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        AppMV()
+        {
+            using (SqlConnection sql = new(connString))
+            {
+                sql.Open();
+                SqlCommand command = new("select * from materials_s_import$", sql)
+            }
+        }
+
+    }
+}
---
+++ session4/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace session4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connString = @"Data Source=DBSRV\SQL2021;Initial Catalog=Zamaraev_Roman;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection sql = new(connString);
+            sql.Open();
+        }
+    }
+}
